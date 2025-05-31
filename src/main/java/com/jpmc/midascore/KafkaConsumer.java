4db07b4c65ceafb1e887b7459aa987e72fdce427
Phantom-VK <vikramadityakhupse@gmail.com>
2025-05-31 14:40:41 +0530

    TestFourPassed Moved transaction processing to TransactionService

--- src/main/java/com/jpmc/midascore/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/KafkaConsumer.java
@@ -1,18 +1,13 @@
 package com.jpmc.midascore;
 
-import com.jpmc.midascore.foundation.Incentive;
-import com.jpmc.midascore.entity.TransactionRecord;
-import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
-import com.jpmc.midascore.repository.TransactionRecordRepository;
-import com.jpmc.midascore.repository.UserRepository;
+import com.jpmc.midascore.services.TransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import static com.jpmc.midascore.foundation.AppConstants.GROUP_ID;
 import static com.jpmc.midascore.foundation.AppConstants.TOPIC;
@@ -23,59 +18,15 @@ public class KafkaConsumer {
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @Autowired
-    private UserRepository userRepo;
-    @Autowired
-    private TransactionRecordRepository txRepo;
-
-    @Autowired
-    private RestTemplate restTemplate;
+    TransactionService transactionService;
 
     @Transactional
     @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
     public void listen(Transaction transaction) {
-        //Find sender by id from our db
-        UserRecord sender = userRepo.findById(transaction.getSenderId());
-        //Find recipient by id from our db
-        UserRecord recipient = userRepo.findById(transaction.getRecipientId());
 
+        transactionService.processTransaction(transaction);
 
-        //Validate transaction
-        if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
-
-            //Get incentive from the incentive service
-            Incentive incentive = restTemplate.postForObject(
-                    "http://localhost:8080/incentive",
-                    transaction,
-                    Incentive.class
-            );
-
-            //Get incentive
-            assert incentive != null;
-            float incentiveAmount = incentive.getAmount();
-
-            //Update sender and recipient balance
-            sender.setBalance(sender.getBalance() - transaction.getAmount());
-            recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
-
-            //Save users
-            userRepo.save(sender);
-            userRepo.save(recipient);
-
-            //We create a new transaction record, and add details to it
-            TransactionRecord txRecord = new TransactionRecord();
-            txRecord.setSender(sender);
-            txRecord.setIncentive(incentiveAmount);
-            txRecord.setRecipient(recipient);
-            txRecord.setAmount(transaction.getAmount());
-
-            //Finally, we save the transaction
-            txRepo.save(txRecord);
-
-
-        }
-
-//        UserRecord wilbur = userRepo.findByname("wilbur");
-//        System.out.println("Wilbur's final balance: " + wilbur.getBalance());
     }
 
 }
+
--- src/main/java/com/jpmc/midascore/services/TransactionService.java
+++ src/main/java/com/jpmc/midascore/services/TransactionService.java
@@ -1,11 +1,73 @@
 package com.jpmc.midascore.services;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 public class TransactionService {
 
     @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private TransactionRecordRepository txRepo;
+
+    @Autowired
     private RestTemplate restTemplate;
 
+    @Transactional
+    public void processTransaction(Transaction transaction) {
+
+        //Find sender by id from our db
+        UserRecord sender = userRepo.findById(transaction.getSenderId());
+        //Find the recipient by id from our db
+        UserRecord recipient = userRepo.findById(transaction.getRecipientId());
+
+        //Validate transaction
+        if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
+
+            //Get incentive
+            float incentiveAmount = getIncentive(transaction);
+            //Update sender balance
+            sender.setBalance(sender.getBalance() - transaction.getAmount());
+
+            //Add incentive to recipient's balance only'
+            recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
+
+            //Save users
+            userRepo.save(sender);
+            userRepo.save(recipient);
+
+            //We create a new transaction record, and add details to it
+            TransactionRecord txRecord = new TransactionRecord();
+            txRecord.setSender(sender);
+            txRecord.setIncentive(incentiveAmount);
+            txRecord.setRecipient(recipient);
+            txRecord.setAmount(transaction.getAmount());
+
+            //Finally, we save the transaction
+            txRepo.save(txRecord);
+
+        }
+
+
+    }
+
+    private float getIncentive(Transaction transaction) {
+        //Get incentive from the incentive service
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:8080/incentive",
+                transaction,
+                Incentive.class
+        );
+
+        //Get incentive
+        assert incentive != null;
+        return incentive.getAmount();
+    }
 }
